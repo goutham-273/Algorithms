67d2b55eae1af7ae3259719f5d6559d85a548219
goutham-273 <86417322+goutham-273@users.noreply.github.com>
2021-06-25 19:28:17 +0530

    Create Staircase.cs

---
+++ Staircase.cs
@@ -0,0 +1,36 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void staircase(int n)
+    {for(var i=n-1;i>=0;i--)
+     {if(i<n-1){Console.WriteLine();}
+      for(var j=0;j<n;j++)
+      {if(j<i){Console.Write(" ");}
+       else{Console.Write("#");}
+      }
+     }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
