55be2ceeaccfaa7f7f31070b7fcfd5db56dcd6c6
goutham-273 <86417322+goutham-273@users.noreply.github.com>
2021-06-25 19:11:53 +0530

    Update and rename problem1.cs to SolveMeFirst.cs

--- SolveMeFirst.cs
+++ SolveMeFirst.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-class Solution {
-    static int solveMeFirst(int a, int b) {
+class Solution
+{
+    static int solveMeFirst(int a, int b)
+    {
       return a+b;
     }
-
-    static void Main(String[] args) {
+ static void Main(String[] args)
+    {
         int val1 = Convert.ToInt32(Console.ReadLine());
         int val2 = Convert.ToInt32(Console.ReadLine());
         int sum = solveMeFirst(val1,val2);
