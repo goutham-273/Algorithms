28291e74da16e008008a9d753117899053392f1d
goutham-273 <86417322+goutham-273@users.noreply.github.com>
2021-06-28 16:19:22 +0530

    Create BirthdayCakeCandles.cs

---
+++ BirthdayCakeCandles.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {candles.Sort();
+     int r=0;
+     for(int i=(candles.Count)-1;i>=0;i--)
+     {if(candles[i]==candles[(candles.Count)-1])
+       {r++;}
+     }
+     return r;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
