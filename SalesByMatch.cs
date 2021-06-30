51d8790e4e4927155a21e8b3bafbe4a5c055eac4
goutham-273 <86417322+goutham-273@users.noreply.github.com>
2021-06-30 21:42:58 +0530

    Create SalesByMatch.cs

---
+++ SalesByMatch.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{   
+    public static int sockMerchant(int n, List<int> ar)
+    {   
+        var ans=0;
+        foreach(var num in ar.GroupBy(i => i))
+        {   
+            ans=ans+(num.Count()/2);
+        }
+        return ans;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
