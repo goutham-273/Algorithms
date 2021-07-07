c358417758dde513e52d1949f1ba68ae1fecd2b8
goutham-273 <86417322+goutham-273@users.noreply.github.com>
2021-07-07 11:39:23 +0530

    Create FindDigits.cs

---
+++ FindDigits.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*https://www.hackerrank.com/challenges/find-digits/problem */
+    public static int findDigits(int n)
+    {
+        var count=0;
+        var m=n;
+        while(n!=0)
+        {
+            var r=n%10;
+            if(r!=0)
+            {
+                if(m%r==0)
+                {
+                    count++;
+                }
+            }
+            n=n/10;
+        }
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.findDigits(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
